460322551a13d9ca745d3ac1ed0b576f15853207
Joel Santos <i3975@rn.senac.local>
2024-03-12 19:31:03 -0300

    Atualização

--- aula-11-03/script.js
+++ aula-11-03/script.js
@@ -5,8 +5,13 @@ let botao = document.getElementById('adicionar')
 function add(event){
     event.preventDefault()
     let item = document.createElement('li')
-    item.textContent = tarefa.value
-    item.addEventListener('click', concluido)
+    item.textContent = tarefa.value // value deve ser usado para receber os dados dos inputs de um form
+    item.addEventListener("click", concluido)
+    let excluir = document.createElement('button')
+    excluir.textContent = "Remover"
+    excluir.classList.add("botaoRemover")
+    excluir.addEventListener('click', remover)
+    item.appendChild(excluir)
     lista.appendChild(item)
     tarefa.value = ""
 }
@@ -14,4 +19,9 @@ botao.addEventListener('click', add)
 
 function concluido(){
   this.classList.add("concluido");
+}
+
+function remover(event){
+  let itemExcluir = event.target.parentElement
+  itemExcluir.remove()
 }
