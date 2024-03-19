7311cb41df444e8cfa855482d7f996a02cd80883
Joel Santos <i3975@rn.senac.local>
2024-03-18 21:19:03 -0300

    localStorage

--- aula-11-03/script2vetor.js
+++ aula-11-03/script2vetor.js
@@ -1,4 +1,5 @@
-let listaDeTarefas = []
+let listaDeTarefas = JSON.parse(localStorage.getItem('banco')) || []
+
 let lista = document.getElementById('lista')
 let tarefa = document.getElementById('tarefa')
 let botao = document.getElementById('adicionar')
@@ -6,26 +7,30 @@ let botao = document.getElementById('adicionar')
 function add(event){
     event.preventDefault()
     if(tarefa.value){
+        //adcionando um item ao vetor listaTarefas
         listaDeTarefas.push(tarefa.value)
         tarefa.value = ""
         atualizarLista()
+        localStorage.setItem('banco', JSON.stringify(listaDeTarefas));
     }else{
         alert('Preencha o campo')
     }
-}
 
+
+}
 function atualizarLista(){
+    lista.innerHTML = ""
     listaDeTarefas.forEach((tarefa, index)=>{
         let item = document.createElement('li')
         item.textContent = tarefa
         let excluir = document.createElement('button')
-        excluir.textContent = "Remover"
+        item.addEventListener("click", concluido)
+        // excluir.textContent = "Remover"
         excluir.classList.add("botaoRemover")
-        excluir.addEventListener('click', remover)
+        excluir.addEventListener('click', function(){remover(index)})
         item.appendChild(excluir)
         lista.appendChild(item)
-    })
-   
+    }) 
 }
 
     // let item = document.createElement('li')
@@ -45,7 +50,13 @@ function concluido(){
   this.classList.add("concluido");
 }
 
-function remover(event){
-  let itemExcluir = event.target.parentElement
-  itemExcluir.remove()
+function remover(index){
+  //let itemExcluir = event.target.parentElement
+  //itemExcluir.remove()
+listaDeTarefas.splice(index, 1)
+localStorage.setItem('banco', JSON.stringify(listaDeTarefas));
+atualizarLista()
+
 }
+
+atualizarLista()
